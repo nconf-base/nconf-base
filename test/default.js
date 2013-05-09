aafb8fa5738c2eeab8fbdaff0119b1b22d3c5776
nconf-base <jimkalac@jims-mac.local>
2013-05-09 17:54:49 -0600

    rename to match project

---
+++ test/default.js
@@ -0,0 +1,10 @@
+/**
+ * Project:     nconf-base
+ * File:
+ * Description:
+ * User:        av4me
+ * Main Repo:   github.com/av4me/nconf-base
+ * Date:        05/09/2013
+ * License:     MIT
+ */
+'use strict';
